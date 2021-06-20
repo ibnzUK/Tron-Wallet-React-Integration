bf8edfd6bdc7d1ee55ecf2e79339f2dd24aae092
Justinas Kairys <info@ibnz.co.uk>
2021-06-20 12:53:18 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Card">
+        <h1> TRONLINK REACT INTEGRATION</h1>
+        <div className="Stats">
+        <h3> WALLET NOT DETECTED</h3>
+        <h4>Account Name:</h4>
+        <h4>My address:</h4>
+        <h4>TRX Balance:</h4>
+        <h4>Tron Network:</h4>
+        <h4>State:</h4>
+        </div>
+      <footer><p>V 0.08 / 2021 &copy; IBNZ DEVELOPERS</p></footer>
+      </div>
     </div>
   );
 }
