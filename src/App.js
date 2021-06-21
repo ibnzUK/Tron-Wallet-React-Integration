35806774f6a7f60489e274e9ed25331d6510501c
Justinas Kairys <info@ibnz.co.uk>
2021-06-21 16:04:48 +0100

    added frozen  balance

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
     name: 'none',
     address: 'none',
     balance: 0,
+    frozenBalance: 0,
     network: 'none',
     link: 'false',
   });
 
   const getBalance = async () => {
-    //if tronlink installed and logged , getting TRX token balance
+    //if wallet installed and logged , getting TRX token balance
     if (window.tronWeb && window.tronWeb.ready) {
       let walletBalances = await window.tronWeb.trx.getAccount(
         window.tronWeb.defaultAddress.base58
@@ -23,43 +24,62 @@ function App() {
     }
   };
 
+  const getWalletDetails = async () => {
+    if (window.tronWeb) {
+      //checking if wallet injected
+      if (window.tronWeb.ready) {
+        let tempBalance = await getBalance();
+        let tempFrozenBalance = 0;
+
+        if (
+          !tempBalance.frozen &&
+          !tempBalance.account_resource.frozen_balance_for_energy
+        ) {
+        } else {
+          if (tempBalance.account_resource.frozen_balance_for_energy) {
+            tempFrozenBalance =
+              tempBalance.account_resource.frozen_balance_for_energy;
+          } else if (tempBalance.frozen) {
+            tempFrozenBalance = tempBalance.frozen[0].frozen_balance;
+          } else {
+            tempFrozenBalance =
+              tempBalance.account_resource.frozen_balance_for_energy +
+              tempBalance.frozen[0].frozen_balance;
+          }
+        }
+
+        //we have wallet and we are logged in
+        setMyMessage(<h3>WALLET CONNECTED</h3>);
+        setMyDetails({
+          name: window.tronWeb.defaultAddress.name,
+          address: window.tronWeb.defaultAddress.base58,
+          balance: tempBalance.balance / 1000000,
+          frozenBalance: tempFrozenBalance / 1000000,
+          network: window.tronWeb.fullNode.host,
+          link: 'true',
+        });
+      } else {
+        //we have wallet but not logged in
+        setMyMessage(<h3>WALLET DETECTED PLEASE LOGIN</h3>);
+        setMyDetails({
+          name: 'none',
+          address: 'none',
+          balance: 0,
+          frozenBalance: 0,
+          network: 'none',
+          link: 'false',
+        });
+      }
+    } else {
+      //wallet is not detected at all
+      setMyMessage(<h3>WALLET NOT DETECTED</h3>);
+    }
+  };
+
   useEffect(() => {
     const interval = setInterval(async () => {
       //
-      if (window.tronWeb) {
-        //checking if wallet injected
-        if (window.tronWeb.ready) {
-          let tempBalance = await getBalance();
-         if(!tempBalance.balance) {
-          tempBalance.balance = 0;
-         } 
-          //we have wallet and we are logged in
-          setMyMessage(<h3>WALLET CONNECTED</h3>);
-          setMyDetails({
-            name: window.tronWeb.defaultAddress.name,
-            address: window.tronWeb.defaultAddress.base58,
-            balance: tempBalance.balance /1000000,
-            network: window.tronWeb.fullNode.host,
-            link: 'true',
-            // balance: await window.tronWeb.trx.getAccount(
-            //   window.tronWeb.defaultAddress.base58
-            // )
-          });
-        } else {
-          //we have wallet but not logged in
-          setMyMessage(<h3>WALLET DETECTED PLEASE LOGIN</h3>);
-          setMyDetails({
-            name: 'none',
-            address: 'none',
-            balance: 0,
-            network: 'none',
-            link: 'false',
-          });
-        }
-      } else {
-        //wallet is not detected at all
-        setMyMessage(<h3>WALLET NOT DETECTED</h3>);
-      }
+      getWalletDetails();
 
       //wallet checking interval 2sec
     }, 2000);
@@ -67,17 +87,20 @@ function App() {
     return () => {
       clearInterval(interval);
     };
-  }, []);
+  });
 
   return (
     <div className="App">
       <div className="Card">
-        <h1> TRONLINK REACT INTEGRATION</h1>
+        <h1> TRON WALLET & REACT INTEGRATION </h1>
         <div className="Stats">
           {myMessage}
           <h4>Account Name: {myDetails.name} </h4>
           <h4>My address: {myDetails.address}</h4>
-          <h4>TRX Balance: {myDetails.balance} TRX</h4>
+          <h4>
+            TRX Balance: {myDetails.balance} TRX (Frozen:{' '}
+            {myDetails.frozenBalance} TRX)
+          </h4>
           <h4>Tron Network: {myDetails.network}</h4>
           <h4>Link established: {myDetails.link}</h4>
         </div>
