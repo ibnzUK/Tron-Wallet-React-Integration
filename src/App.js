976a987cbbfe1cbb5a120b997ddfc594bde682fe
Justinas Kairys <info@ibnz.co.uk>
2021-06-21 12:20:15 +0100

    optimised fetching

--- src/App.js
+++ src/App.js
@@ -2,62 +2,66 @@ import { React, useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [tronLinkState, setTronLinkState] = useState('null');
-  const [connectedWallet, setconnectedWallet] = useState([
-    {
-      address: 'null',
-      name: 'null',
-    },
-  ]);
-  const [accountBalance, setAccountBalance] = useState(0);
-  const [walletAddress, setWalletAddress] = useState('null');
-  const [networkState, setNetworkState] = useState('null');
-  const [myMessage, setMyMessage] = useState(<h3> CHECKING </h3>);
-  let tradeobj = 'nne';
+  const [myMessage, setMyMessage] = useState(<h3> LOADING.. </h3>);
+  const [myDetails, setMyDetails] = useState({
+    name: 'none',
+    address: 'none',
+    balance: 0,
+    network: 'none',
+    link: 'false',
+  });
 
-  const connectTronlink = async () => {
-    await new Promise((resolve) => {
-      const tronWebState = {
-        installed: !!window.tronWeb,
-        loggedIn: window.tronWeb && window.tronWeb.ready,
-      };
+  const getBalance = async () => {
+    //if tronlink installed and logged , getting TRX token balance
+    if (window.tronWeb && window.tronWeb.ready) {
+      let walletBalances = await window.tronWeb.trx.getAccount(
+        window.tronWeb.defaultAddress.base58
+      );
+      return walletBalances;
+    } else {
+      return 0;
+    }
+  };
 
-      if (tronWebState.installed) {
-        setTronLinkState(tronWebState.LoggedIn);
-        return resolve();
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      //
+      if (window.tronWeb) {
+        //checking if wallet injected
+        if (window.tronWeb.ready) {
+          let tempBalance = await getBalance();
+         if(!tempBalance.balance) {
+          tempBalance.balance = 0;
+         } 
+          //we have wallet and we are logged in
+          setMyMessage(<h3>WALLET CONNECTED</h3>);
+          setMyDetails({
+            name: window.tronWeb.defaultAddress.name,
+            address: window.tronWeb.defaultAddress.base58,
+            balance: tempBalance.balance /1000000,
+            network: window.tronWeb.fullNode.host,
+            link: 'true',
+            // balance: await window.tronWeb.trx.getAccount(
+            //   window.tronWeb.defaultAddress.base58
+            // )
+          });
+        } else {
+          //we have wallet but not logged in
+          setMyMessage(<h3>WALLET DETECTED PLEASE LOGIN</h3>);
+          setMyDetails({
+            name: 'none',
+            address: 'none',
+            balance: 0,
+            network: 'none',
+            link: 'false',
+          });
+        }
+      } else {
+        //wallet is not detected at all
+        setMyMessage(<h3>WALLET NOT DETECTED</h3>);
       }
-   
-    });
 
-  };
-  const loadData = async () => {
-    setconnectedWallet([
-      {
-        address: window.tronWeb.defaultAddress.base58,
-        name: window.tronWeb.defaultAddress.name,
-      },
-    ]);
-    setWalletAddress(window.tronWeb.defaultAddress.base58);
-    tradeobj = await window.tronWeb.trx.getAccount(
-      window.tronWeb.defaultAddress.base58
-    );
-
-    setNetworkState(window.tronWeb.fullNode.host);
-    setTronLinkState('True');
-    setMyMessage(<h3> WALLET DETECTED</h3>);
-
-    setAccountBalance((await tradeobj.balance) / 1000000);
-  };
-
-  useEffect(() => {
-    connectTronlink();
-  }, []);
-
-  useEffect(() => {
-    connectTronlink();
-    //starting temp monitoring every 1 sec
-    const interval = setInterval(() => {
-      loadData();
+      //wallet checking interval 2sec
     }, 2000);
 
     return () => {
@@ -71,21 +75,14 @@ function App() {
         <h1> TRONLINK REACT INTEGRATION</h1>
         <div className="Stats">
           {myMessage}
-          <h4>Account Name: {connectedWallet[0].name} </h4>
-          <h4>My address: {walletAddress}</h4>
-          <h4>TRX Balance: {accountBalance} TRX</h4>
-          <h4>Tron Network: {networkState}</h4>
-          <h4>Link established: {tronLinkState}</h4>
+          <h4>Account Name: {myDetails.name} </h4>
+          <h4>My address: {myDetails.address}</h4>
+          <h4>TRX Balance: {myDetails.balance} TRX</h4>
+          <h4>Tron Network: {myDetails.network}</h4>
+          <h4>Link established: {myDetails.link}</h4>
         </div>
-        {/* <button
-          onClick={() => {
-            loadData();
-          }}
-        >
-          Do Something
-        </button> */}
         <footer>
-          <p>V 0.01 / 2021 &copy; IBNZ DEVELOPERS</p>
+          <p>V 0.02 / 2021 &copy; IBNZ DEVELOPERS</p>
         </footer>
       </div>
     </div>
